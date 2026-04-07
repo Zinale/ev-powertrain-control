b287078c6dc65be179e7eda48bec08b9bcfc68b2
AlessandroZingaretti <zingaale@gmail.com>
2026-04-07 15:26:49 +0200

    Update TVC (a better Car model is required!)

--- mcu-stm32/Release/Core/Src/Tasks/Tasks.d
+++
@@ -1,6 +0,0 @@
-Core/Src/Tasks/Tasks.o: ../Core/Src/Tasks/Tasks.c \
- E:/UNIVPM/PolimarcheRacingTeam/FRONT/front-PeacockElettrica/MCU/mcu-stm32/Core/Inc/Task/Tasks.h \
- ../Core/Inc/Config.h ../Core/Inc/Inverter/Inverter.h
-E:/UNIVPM/PolimarcheRacingTeam/FRONT/front-PeacockElettrica/MCU/mcu-stm32/Core/Inc/Task/Tasks.h:
-../Core/Inc/Config.h:
-../Core/Inc/Inverter/Inverter.h:
